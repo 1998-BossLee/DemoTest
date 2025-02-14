f957591d8a32454547d47609abb3e2c5168002ca
liyangjin <8898764+shoulinniao@user.noreply.gitee.com>
2025-02-14 13:44:27 +0800

    test

--- java/web3/MoreGmail.java
+++ java/web3/MoreGmail.java
@@ -8,60 +8,37 @@ public class MoreGmail {
         String email = "guanlaoshu1@gmail.com";
         int plusVariationCount = 20; // 生成 5 个+裂变
 
-        Set<String> variations = generateVariations(email, plusVariationCount);
-
-        for (String variant : variations) {
-            System.out.println(variant);
+        List<String> emails = generateVariations(email, plusVariationCount);
+        for (String s : emails) {
+            System.out.println(s);
         }
     }
-
-    public static Set<String> generateVariations(String email, int plusVariationCount) {
-        Set<String> variations = new HashSet<>();
-
+    public static List<String> generateVariations(String email, int plusVariationCount) {
         String localPart = email.split("@")[0];
         String domain = email.split("@")[1];
-
         // 大小写裂变
-        //variations.addAll(generateCaseVariations(localPart, domain));
-
-        // +裂变
-        variations.addAll(generatePlusVariations(localPart, domain, plusVariationCount));
-
-        return variations;
+        return generateCaseVariations(localPart, domain);
     }
 
     // 生成大小写裂变
-    private static Set<String> generateCaseVariations(String localPart, String domain) {
+    private static List<String> generateCaseVariations(String localPart, String domain) {
         Set<String> caseVariations = new HashSet<>();
         char[] chars = localPart.toCharArray();
-
+        List<String> list = new ArrayList<>();
         for (int length = 1; length <= chars.length; length++) { // 1 到 N 连续大写
             for (int start = 0; start <= chars.length - length; start++) {
                 char[] temp = Arrays.copyOf(chars, chars.length);
                 for (int i = start; i < start + length; i++) {
                     temp[i] = Character.toUpperCase(temp[i]);
                 }
-                caseVariations.add(new String(temp) + "@" + domain);
+                String email = new String(temp) + "@" + domain;
+                if (caseVariations.add(email)) {
+                    list.add(email);
+                }
             }
         }
-
-        return caseVariations;
+        return list;
     }
 
-    // 生成 + 裂变
-    private static Set<String> generatePlusVariations(String localPart, String domain, int count) {
-        Set<String> plusVariations = new HashSet<>();
-        Random random = new Random();
-        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-        for (int i = 0; i < count; i++) {
-            StringBuilder plusSuffix = new StringBuilder("+");
-            for (int j = 0; j < 5; j++) { // 生成 5 个随机字符
-                plusSuffix.append(chars.charAt(random.nextInt(chars.length())));
-            }
-            plusVariations.add(localPart + plusSuffix + "@" + domain);
-        }
-
-        return plusVariations;
-    }
 }
