e0895a715ecf1eb75143f96c83547bbfad24f3fb
1998-BossLee <772013350@qq.com>
2025-02-17 10:28:15 +0800

    邮箱大小写裂变

--- java/web3/MoreGmail.java
+++ java/web3/MoreGmail.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class MoreGmail {
 
     public static void main(String[] args) {
-        String email = "lichenyao2@gmail.com";
+        String email = "zhangxinyu@gmail.com";
 
         List<String> emails = generateVariations(email);
         for (String s : emails) {
