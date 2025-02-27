f7c9d2edbedcbfbcaa6731e8568420027aec7efd
liyangjin <772013350@qq.com>
2025-02-27 14:43:35 +0800

    刷提交

--- java/web3/MoreGmail.java
+++ java/web3/MoreGmail.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class MoreGmail {
 
     public static void main(String[] args) {
-        String email = "guanlao@gmail.com";
+        String email = "guanfhaiuslao@gmail.com";
 
         List<String> emails = generateVariations(email);
         for (String s : emails) {
